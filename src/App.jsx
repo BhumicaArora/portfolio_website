bbb7a432ecef02ef0de8baba540ba96266cd6183
Bhumica Arora <bhumicaarora16@gmail.com>
2025-08-12 10:01:00 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Navbar from "./components/Navtemp/navbar"
+import Navbar from "./components/Navbar/Navbar"
 import About from "./components/About/About"
 import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
